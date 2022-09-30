6b31a91efe67fbbb3ba4810fd688646783b188f8
Marsel <marchello.marsel@gmail.com>
2022-09-30 17:39:46 +0600

    добавил регулярные выражения в данных для слайдера

--- js/script.js
+++ js/script.js
@@ -357,11 +357,15 @@ window.addEventListener('DOMContentLoaded', () => {
 		dots[slideIndex - 1].style.opacity = 1;
 	}
 
+	function deleteNotDigits(str) {
+		return +str.replace(/\D/g, '');
+	}
+
 	next.addEventListener('click', () => {
-		if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+		if (offset == deleteNotDigits(width) * (slides.length - 1)) {
 			offset = 0;
 		} else {
-			offset += +width.slice(0, width.length - 2);
+			offset += deleteNotDigits(width);
 		}
 		slidesField.style.transform = `translateX(-${offset}px)`;
 		if (slideIndex == slides.length) {
@@ -374,9 +378,9 @@ window.addEventListener('DOMContentLoaded', () => {
 	});
 	prev.addEventListener('click', () => {
 		if (offset == 0) {
-			offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+			offset = deleteNotDigits(width) * (slides.length - 1);
 		} else {
-			offset -= +width.slice(0, width.length - 2);
+			offset -= deleteNotDigits(width);
 		}
 		slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -394,7 +398,7 @@ window.addEventListener('DOMContentLoaded', () => {
 		dot.addEventListener('click', (e) => {
 			const slideTo = e.target.getAttribute('data-slide-to');
 			slideIndex = slideTo;
-			offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+			offset = deleteNotDigits(width) * (slideTo - 1);
 
 			slidesField.style.transform = `translateX(-${offset}px)`;
 
